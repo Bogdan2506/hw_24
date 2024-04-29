38c4053f33be63e2b704e664b5d3f16859c003a8
Bogdan <kovalskiy256@gmail.com>
2024-04-29 18:14:53 +0300

    HW_24

---
+++ js/data.js
@@ -0,0 +1,15 @@
+const defaultUsers = [{
+        id: "1",
+        name: 'John Smith',
+        login: 'jsmith',
+        email: 'john.smith@ithillel.ua',
+        age: 30,
+    },
+    {
+        id: "2",
+        name: 'Stacie Mcdonald',
+        login: 'smcdonald',
+        email: 'stacie.mcdonald@ithillel.ua',
+        age: 30,
+    },
+];
